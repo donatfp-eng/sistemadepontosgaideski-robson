70699d7a9d4d846e8a5ebfe56ebe6d8e63d6a366
donatfp-eng <donatfp@gmail.com>
2026-05-29 15:36:25 -0300

    Update _db.ts

--- gaideski-metas/api/_db.ts
+++ gaideski-metas/api/_db.ts
@@ -2,5 +2,5 @@ import { drizzle } from 'drizzle-orm/neon-http'
 import { neon } from '@neondatabase/serverless'
 import * as schema from './schema'
 
-const sql = neon(process.env.DATABASE_URL!)
-export const db = drizzle(sql, { schema })
+const sql = neon(process.env.DATABASE_URL as string)
+export const db = drizzle(sql as any, { schema })
